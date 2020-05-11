c029e1f2025f2523a198553dccff91764abde929
Michael Nutt <michael@nuttnet.net>
2020-05-11 09:36:53 -0400

    eslint our js

--- js/filter-stream.js
+++ js/filter-stream.js
@@ -1,26 +1,26 @@
-import { transform } from "./pose.js";
+import { transform } from './pose.js';
 
 class FilterStream {
   constructor(stream) {
     this.stream = stream;
-    const video = document.createElement("video");
+    const video = document.createElement('video');
 
     video.srcObject = stream;
     this.video = video;
   }
 
   async setupStream() {
-    console.log("Starting up video stream");
+    console.log('Starting up video stream');
     await this.videoIsReady();
-    console.log("Video stream is active");
+    console.log('Video stream is active');
 
     return transform(this.video);
   }
 
   videoIsReady() {
     return new Promise((resolve, reject) => {
-      this.video.addEventListener("playing", resolve);
-      this.video.addEventListener("error", reject);
+      this.video.addEventListener('playing', resolve);
+      this.video.addEventListener('error', reject);
       this.video.play();
     });
   }
